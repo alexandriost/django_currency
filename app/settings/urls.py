4e2e0af08bfe07fb2536c98211efcea22c53f521
Oleksandr Ostrolutsky <oleksandr.ostrolutsky@gmail.com>
2023-02-20 21:47:37 +0200

    Added ContactUs to models

--- app/settings/urls.py
+++ app/settings/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from main.views import hello_world
+from currency.views import list_rates, list_contactus
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello-world/', hello_world),
+    path('rate/list', list_rates),
+    path('contactus/list', list_contactus)
 ]
